263521d89de58bd1eefcc1b5458a86a74876c0e7
Marko Bieliajew <adm96.24@gmail.com>
2019-10-23 21:50:56 +0200

    playlist creator

---
+++ M3U list creater/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace M3U_list_creater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Folder with files:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 396);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(547, 12);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Title",
+            "Artist",
+            "Album",
+            "Duration"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(196, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sort by:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 118);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 272);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "M3U list|*.m3u";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 420);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "M3U list Creater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ M3U list creater/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TagLib;
+
+namespace M3U_list_creater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void SortBy(List<string> str, int col)
+        {
+            List<string> tmp = new List<string>();
+            int i = 0;
+            progressBar1.Value = 0;
+            progressBar1.Maximum = str.Count;
+            while (i < str.Count)
+            {
+                if (!System.IO.File.Exists(str[i]))
+                {
+                    str.RemoveAt(i);
+                    i++;
+                    continue;
+                }
+                bool valid = false;
+                if (str[i].ToLower().Contains(".flac")) valid = true;
+                if (str[i].ToLower().Contains(".mp3")) valid = true;
+                if (str[i].ToLower().Contains(".wav")) valid = true;
+                if (str[i].ToLower().Contains(".aac")) valid = true;
+                if (str[i].ToLower().Contains(".ape")) valid = true;
+                if (str[i].ToLower().Contains(".ogg")) valid = true;
+                if (!valid)
+                {
+                    str.RemoveAt(i);
+                    i++;
+                    continue;
+                }
+                TagLib.File file = TagLib.File.Create(str[i]);
+                if(file != null)
+                {
+                    switch(col)
+                    {
+                        case 0: tmp.Add(file.Tag.Title); break;
+                        case 1: tmp.Add(file.Tag.FirstPerformer); break;
+                        case 2: tmp.Add(file.Tag.Album + " " + file.Tag.Disc.ToString()); break;
+                        case 3: tmp.Add(Convert.ToInt32(file.Properties.Duration.TotalSeconds).ToString()); break;
+                        default: tmp.Add(file.Tag.Title); break;
+                    }
+                    tmp[tmp.Count - 1] = tmp[tmp.Count - 1] + "|" + str[i]; 
+                }
+                else
+                {
+                    str.RemoveAt(i);
+                }
+                i++;
+                progressBar1.Value = progressBar1.Value + 1;
+                progressBar1.Maximum = str.Count;
+            }
+            tmp.Sort();
+            str.Clear();
+            for(i = 0; i < tmp.Count;i++)
+            {
+                str.Add(tmp[i].Split('|')[1]);
+            }
+            tmp.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text) != true)
+                return;
+            List<string> tmp = new List<string>(Directory.GetFiles(textBox1.Text));
+            List<string> fin = new List<string>();
+
+            SortBy(tmp,comboBox1.SelectedIndex);
+            progressBar1.Value = 0;
+            progressBar1.Maximum = tmp.Count;
+            fin.Add("#EXTM3U");
+            for(int i = 0; i < tmp.Count;i++)
+            {
+                TagLib.File file = TagLib.File.Create(tmp[i]);
+                fin.Add("#EXTINF:"+ Convert.ToInt32(file.Properties.Duration.TotalSeconds).ToString() + ","+file.Tag.Performers.ToString()+" - "+file.Tag.Title);
+                fin.Add(Path.GetFileName(file.Name));
+                progressBar1.Value = progressBar1.Value + 1;
+            }
+            richTextBox1.Clear();
+            richTextBox1.Lines = fin.ToArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (Directory.Exists(folderBrowserDialog1.SelectedPath))
+                    textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllLines(saveFileDialog1.FileName,richTextBox1.Lines);        
+            }
+        }
+    }
+}
